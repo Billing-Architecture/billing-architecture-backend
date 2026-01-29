636ee0b988150efa874c2446c1af1f70f8c8d261
dennis-gomez <dennis.gomez1310@gmail.com>
2026-01-29 13:17:02 -0600

    Logic Payment
    Modification on PaymentsServiceJPA on savePayment service, to validate and excecute payment, to avoid overpayments.

--- billing-service/src/main/java/com/billing/service/billing_service/jpa/PaymentsServiceJPA.java
+++ billing-service/src/main/java/com/billing/service/billing_service/jpa/PaymentsServiceJPA.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.billing.service.billing_service.domain.Bills;
 import com.billing.service.billing_service.domain.Payments;
+import com.billing.service.billing_service.repositorys.OrdersRepository;
 import com.billing.service.billing_service.repositorys.PaymentsRepository;
 import com.billing.service.billing_service.services.IPaymentsService;
 
@@ -17,8 +20,35 @@ public class PaymentsServiceJPA implements IPaymentsService{
     @Autowired
     PaymentsRepository repository;
 
+    @Autowired
+    OrdersRepository repositoryOrder;
+
     @Override
+    @Transactional
     public void savePayment(Payments payment) {
+        Bills bill = payment.getBill();
+        double totalPaid = 0; 
+        double totalPayment = payment.getPaymentTotal();
+
+        for(Payments p : bill.getPayments()){
+            totalPaid += p.getPaymentTotal();
+        }
+
+        double newPayment = payment.getPaymentTotal();
+        double billTotal = bill.getBillTotal();
+
+        if (totalPaid > (totalPaid + newPayment)) {
+            throw new RuntimeException("El pago excede el total de la factura");
+        }
+        
+        bill.getPayments().add(payment);
+        bill.setBillTotalPaid(totalPaid + totalPayment);
+
+        if (bill.getBillTotalPaid() == billTotal) {
+            bill.setBillState("PAID");
+        } else {
+            bill.setBillState("PARTIAL");
+        }
         repository.save(payment);
     }
 
@@ -26,5 +56,4 @@ public class PaymentsServiceJPA implements IPaymentsService{
     public List<Payments> getPayment() {
         return repository.findAll();
     }
-
 }
