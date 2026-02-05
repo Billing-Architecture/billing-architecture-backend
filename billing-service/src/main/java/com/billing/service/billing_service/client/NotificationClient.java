becd83df0566aa135e712a11099efe3beaf2454f
dennis-gomez <dennis.gomez1310@gmail.com>
2026-02-05 09:47:34 -0600

    Connection to endpoints
    Implementation of endpoints to connect to the notification service.

--- billing-service/src/main/java/com/billing/service/billing_service/client/NotificationClient.java
+++ billing-service/src/main/java/com/billing/service/billing_service/client/NotificationClient.java
@@ -1,5 +1,6 @@
 package com.billing.service.billing_service.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,13 +12,13 @@ import com.billing.service.billing_service.dtos.PaymentNotificationDTO;
 public class NotificationClient {
     private final WebClient webClient;
 
-    public NotificationClient (WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("").build(); // configuration of webclient
+    public NotificationClient (WebClient.Builder builder, @Value("${notification.service.url}") String baseUrl) {
+        this.webClient = builder.baseUrl(baseUrl).build(); // configuration of webclient
     }
 
     public void sendNotification (BillNotificationDTO notification) {
         webClient.post()
-            .uri("")
+            .uri("api/notify/bill")
             .contentType(MediaType.APPLICATION_JSON)
             .bodyValue(notification)
             .retrieve() //Execute the petition and wait the server
@@ -27,7 +28,7 @@ public class NotificationClient {
 
     public void sendNotification (PaymentNotificationDTO notification) {
         webClient.post()
-            .uri("")
+            .uri("api/notify/payment")
             .contentType(MediaType.APPLICATION_JSON)
             .bodyValue(notification)
             .retrieve()
